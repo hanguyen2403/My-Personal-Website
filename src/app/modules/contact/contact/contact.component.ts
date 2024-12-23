90d4708c799073f2ee452a02217f787a5584a6ac
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-12-23 22:34:41 +0700

    add logic send message

--- src/app/modules/contact/contact/contact.component.ts
+++ src/app/modules/contact/contact/contact.component.ts
@@ -1,19 +1,55 @@
 import { Component } from '@angular/core';
+import emailjs from '@emailjs/browser';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-contact',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
   isSendMsg: boolean = false;
-    
-  sendMessage(event: Event) {
-    event.preventDefault(); // Prevent form submission
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  sendMessage(event: Event): void {
     this.isSendMsg = true;
-    setTimeout(() => {
-      this.isSendMsg = false;
-    }, 3000); // Reset state after 4 seconds
+    event.preventDefault();
+
+    if (this.contactForm.valid) {
+      emailjs
+        .send(
+          'service_8e03vd3', // Replace with your EmailJS service ID
+          'template_8k8pizq', // Replace with your EmailJS template ID
+          {
+            from_name: this.contactForm.value.name,
+            reply_to: this.contactForm.value.email,
+            subject: this.contactForm.value.subject,
+            message: this.contactForm.value.message,
+          },
+          'eramHKTjS6t49ArHp' // Replace with your EmailJS public key
+        )
+        .then(
+          (response) => {
+            console.log('Email sent successfully:', response.status, response.text);
+            this.contactForm.reset();
+            this.isSendMsg = false;
+          },
+          (error) => {
+            console.error('Failed to send email:', error);
+          }
+        );
+    } else {
+      console.error('Form is invalid');
+    }
   }
 }
